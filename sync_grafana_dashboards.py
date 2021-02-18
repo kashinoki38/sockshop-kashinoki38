dab14e2da5fe48c53746bd0c5f5abd3f46d7e613
kashinoki38 <kashinoki38@gmail.com>
2021-02-19 07:08:50 +0900

    modify - sync_grafana_dashboards.py for reading local json - change PromQL

--- sync_grafana_dashboards.py
+++ sync_grafana_dashboards.py
@@ -52,49 +52,49 @@ charts = [
     #     'max_kubernetes': '1.14.0-0'
     # },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/pod-detail-dashboard.json',
+        'source': 'grafana-dashboards-base/pod-detail-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/istio-mesh-dashboard.json',
+        'source': 'grafana-dashboards-base/istio-mesh-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/istio-control-plane-dashboard.json',
+        'source': 'grafana-dashboards-base/istio-control-plane-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/istio-service-dashboard.json',
+        'source': 'grafana-dashboards-base/istio-service-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/istio-workload-dashboard.json',
+        'source': 'grafana-dashboards-base/istio-workload-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/jmx-exporter-dashboard.json',
+        'source': 'grafana-dashboards-base/jmx-exporter-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/kubernetes-cluster-dashboard.json',
+        'source': 'grafana-dashboards-base/kubernetes-cluster-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
     },
     {
-        'source': 'https://raw.githubusercontent.com/kashinoki38/sockshop-kashinoki38/grafana/grafana-dashboards-base/node-exporter-dashboard.json',
+        'source': 'grafana-dashboards-base/node-exporter-dashboard.json',
         'destination': 'kube-prometheus-stack/templates/grafana/dashboards-1.14',
         'type': 'json',
         'min_kubernetes': '1.14.0-0'
@@ -244,11 +244,15 @@ def main():
     # read the rules, create a new template file per group
     for chart in charts:
         print("Generating rules from %s" % chart['source'])
-        response = requests.get(chart['source'])
-        if response.status_code != 200:
-            print('Skipping the file, response code %s not equals 200' % response.status_code)
-            continue
-        raw_text = response.text
+        # response = requests.get(chart['source'])
+        # if response.status_code != 200:
+            # print('Skipping the file, response code %s not equals 200' % response.status_code)
+            # continue
+        # raw_text = response.text
+        file_path = chart['source']
+        with open(file_path,"r", newline='\n') as inF:
+            raw_text = inF.read()
+        inF.close()
 
         if ('max_kubernetes' not in chart):
             chart['max_kubernetes']="9.9.9-9"
